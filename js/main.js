889c7d019d308f015d2e969406cc969e4e426cff
Courtney Fay <cfay@Courtneys-MacBook-Air-2.local>
2017-08-22 23:57:22 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,17 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[1];
+cardsInPlay.push(cardOne);
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardOne === cardTwo) {
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+}
